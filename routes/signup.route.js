74e576fbbc6d97fcee4893f6e98f32d903cce6a7
Ojo Morakinyo Samuel <arayosam@gmail.com>
2023-07-06 12:37:24 +0100

    saved now

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -5,12 +5,15 @@ const router = express.Router()
 router.get("/signup", (req, res) => {
     res.send()
 })
+
 router.get("/signin", (req, res) => {
     res.send()
 })
+
 router.get("/users", (req, res) => {
     res.send()
 })
+
 
 
 module.exports = router
